0b05f49800c88b10e29d0de6471d612df1bcf68e
Diep Pham <imeo@favadi.com>
2016-08-03 18:03:23 +0700

    first commit

---
+++ generate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+protoc -I/usr/local/include -I. \
+       -I"$GOPATH"/src \
+       -I"$GOPATH"/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+       --grpc-gateway_out=logtostderr=true:. \
+       pb/example.proto
---
+++ install_protobuf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+protobuf_url='https://github.com/google/protobuf/archive/v3.0.0.tar.gz'
+protobuf3_dir=~/protobuf3
+
+apt-get install -y --no-install-recommends \
+        curl \
+        dh-autoreconf \
+        unzip
+
+wget "$protobuf_url" -O protobuf3.tar.gz
+tar -xzf protobuf3.tar.gz -C $protobuf3_dir --strip 1
+rm protobuf3.tar.gz
+pushd $protobuf3_dir
+./autogen.sh
+./configure --prefix=/usr
+make
+make install
+popd
